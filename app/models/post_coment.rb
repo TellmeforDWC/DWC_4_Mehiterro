aef49a3eeb15a933053fbe0d432fdbad53691ba4
TellmeforDWC <rikugame1003@gmail.com>
2021-09-15 15:11:55 +0000

    [update]Meshiterroの作成：コメント投稿機能実装中

--- app/models/post_coment.rb
+++ app/models/post_coment.rb
@@ -1,2 +1,6 @@
 class PostComent < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :post_image
+
 end
--- db/migrate/20210915141722_create_post_coments.rb
+++ db/migrate/20210915141722_create_post_coments.rb
@@ -2,7 +2,6 @@ class CreatePostComents < ActiveRecord::Migration[5.2]
   def change
     create_table :post_coments do |t|
       t.integer :user_id
-      t.integer :id
       t.integer :post_image_id
       t.text :comment
 
