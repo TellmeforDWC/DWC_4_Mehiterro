3c0e332eae4bad97963f8582ba4e9a88de40fd98
TellmeforDWC <rikugame1003@gmail.com>
2021-09-17 10:05:06 +0000

    [update]Meshiterroの作成：コメント機能、イイね！機能の実装完了

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -4,12 +4,14 @@ class PostCommentsController < ApplicationController
     post_image = PostImage.find(params[:post_image_id])
     comment = PostComment.new(post_comment_params)
     comment.user_id = current_user.id
-    commnet.post_image_id = post_image_id
+    comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image)
   end
 
   def destroy
+    PostComment.find_by(id: params[:id]).destroy
+    redirect_to post_image_path(params[:post_image_id])
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_141722) do
+ActiveRecord::Schema.define(version: 2021_09_17_080257) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_coments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_image_id"
     t.text "comment"
